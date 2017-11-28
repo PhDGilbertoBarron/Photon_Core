8b5adbb2082b1032a00c841576e74241bbba8350
Hector Gilberto Barron Gonzalez <phdgilbertobarron@gmail.com>
2017-11-27 22:38:29 -0600

    Blink en D7
    Se agrega el primer codigo utilizado en la clase de sistemas embebidos, este codigo genera un estado blink en el led d7.

---
+++ Blink_d7/Blink_d7.cpp
@@ -0,0 +1,14 @@
+int LED = D7;
+
+void setup()
+{   
+    pinMode(LED, OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(LED, HIGH);
+    delay(2000);
+    digitalWrite(LED, LOW);
+    delay(2000);
+}
